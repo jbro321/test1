f53e45890d4a10eb47f9213b62574d5e4c7a2b67
JP <jaehyung0321@gmail.com>
2021-06-29 17:37:59 +0900

    JP_test1

--- JP_test1/jpuser/admin.py
+++ JP_test1/jpuser/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import JPuser
 
 # Register your models here.
+
+class JPuserAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(JPuser, JPuserAdmin)
