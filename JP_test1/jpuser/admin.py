b27195260b7bf00810254edd3968fd030ce7cf5d
JaeHyung Park <jaehyung0321@gmail.com>
2021-06-30 19:11:06 +0900

    Delete JP_test1 directory

--- JP_test1/jpuser/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import JPuser
-
-# Register your models here.
-
-class JPuserAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(JPuser, JPuserAdmin)
